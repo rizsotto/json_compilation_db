937f28803ea5ae1be15f8130e1427a7d9a15bdec
Laszlo Nagy <rizsotto@gmail.com>
2019-10-21 17:58:57 +1100

    add api documentation

--- src/lib.rs
+++ src/lib.rs
@@ -21,10 +21,14 @@ mod error {
     use std::fmt;
     use std::io;
 
+    /// This error type encompasses any error that can be returned by this crate.
     #[derive(Debug)]
     pub enum Error {
+        /// Represents basic IO failure.
         IoError(io::Error),
+        /// Represents JSON read or write failure.
         SyntaxError(serde_json::Error),
+        /// Represents semantic failure during procession of the file content.
         SemanticError(String),
     }
 
@@ -78,18 +82,36 @@ mod api {
     /// Represents an entry of the compilation database.
     #[derive(Debug, PartialEq)]
     pub struct Entry {
+        /// The main translation unit source processed by this compilation step.
+        /// This is used by tools as the key into the compilation database.
+        /// There can be multiple command objects for the same file, for example if the same
+        /// source file is compiled with different configurations.
         pub file: path::PathBuf,
+        /// The compile command executed. After JSON unescaping, this must be a valid command
+        /// to rerun the exact compilation step for the translation unit in the environment
+        /// the build system uses. Shell expansion is not supported.
         pub command: Vec<String>,
+        /// The working directory of the compilation. All paths specified in the command or
+        /// file fields must be either absolute or relative to this directory.
         pub directory: path::PathBuf,
+        /// The name of the output created by this compilation step. This field is optional.
+        /// It can be used to distinguish different processing modes of the same input file.
         pub output: Option<path::PathBuf>,
     }
 
     /// Represents the content of the compilation database.
+    ///
+    /// A compilation database is a JSON file, which consist of an array of “command objects”,
+    /// where each command object specifies one way a translation unit is compiled in the project.
     pub type Entries = Vec<Entry>;
 
     /// Represents the expected format of the JSON compilation database.
     #[derive(Debug, PartialEq, Eq)]
     pub struct Format {
+        /// Controls which field to emit in the final database.
+        ///
+        /// In the output the field `command` is a string and `arguments` is an array of
+        /// strings. Either `command` or `arguments` is required.
         pub command_as_array: bool,
     }
 
@@ -101,18 +123,22 @@ mod api {
         }
     }
 
+    /// The conventional name for a compilation database file which tools are looking for.
     pub const DEFAULT_FILE_NAME: &str = "compile_commands.json";
 
+    /// Load the content of the given file and parse it as a compilation database.
     pub fn load_from_file(file: &path::Path) -> Result<Entries, Error> {
         let reader = fs::OpenOptions::new().read(true).open(file)?;
 
         load_from_reader(reader)
     }
 
+    /// Load the content of the given stream and parse it as a compilation database.
     pub fn load_from_reader(reader: impl io::Read) -> Result<Entries, Error> {
         inner::load_from_reader(reader)
     }
 
+    /// Persists the entries into the given file name with the given format.
     pub fn save_into_file(
         file: &path::Path,
         entries: Entries,
@@ -127,6 +153,7 @@ mod api {
         save_into_writer(writer, entries, format)
     }
 
+    /// Persists the entries into the given stream with the given format.
     pub fn save_into_writer(
         writer: impl io::Write,
         entries: Entries,
